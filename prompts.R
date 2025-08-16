22591a904882b7fef9de70f2bbfb3de022e081ed
Davide <gentiledv@gmail.com>
2025-08-15 20:58:20 -0400

    aug edits

---
+++ prompts.R
@@ -0,0 +1,5 @@
+# prompts 
+
+You have a double Ita and Can passport. You have PhD in human-AI evaluation and human factors in nuclear, from uunivesity of toronto. currently are
+human factors analyst at kinectrics in toronto. Your goal is to optimize lifestyle, income growth. 
+Design a website for this. 
